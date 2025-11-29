39490976242e951ba233a83ed190b2aa5389c0ac
Raheem Ullah <raheemullah8@user.noreply.gitub.com>
2025-11-29 00:04:23 -0800

    feat: complete homepage with git commands section

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
     <div className="home-container">
       <h1>Welcome to Our Website</h1>
       <p>This is the homepage created using Git branches!</p>
+      
       <div className="features">
         <div className="feature-card">
           <h3>Feature 1</h3>
@@ -19,6 +20,17 @@ function Home() {
           <h3>Feature 3</h3>
           <p>Master GitHub workflow</p>
         </div>
+      </div>
+      
+      {/* New section add karo */}
+      <div className="git-info">
+        <h2>Git Commands We Learned:</h2>
+        <ul>
+          <li>git stash - Temporary save</li>
+          <li>git stash pop - Restore saved changes</li>
+          <li>git checkout -b - New branch</li>
+          <li>git merge - Branch combine</li>
+        </ul>
       </div>
     </div>
   );
